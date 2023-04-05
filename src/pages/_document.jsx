999d260aa5b6066f8f364c53d6b627589735ff0b
narmit <narmitmashruwala@gmail.com>
2023-04-05 00:47:12 -0400

    fix: build errors

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -5,7 +5,11 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin={true}
+        />
 
         {/* Font - Roboto */}
         <link
---
+++ src/pages/search/index.jsx
@@ -0,0 +1,14 @@
+import MainLayout from "@components/layouts/main-layout";
+import React from "react";
+
+const Search = () => {
+  return (
+    <>
+      <MainLayout>
+        <div>search page</div>
+      </MainLayout>
+    </>
+  );
+};
+
+export default Search;
