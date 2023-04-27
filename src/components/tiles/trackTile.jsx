ea48762d4f99495dbdd3777be68abdb647333fe9
narmit <narmitmashruwala@gmail.com>
2023-04-27 09:03:02 -0400

    feat: add play btn

--- src/components/tiles/trackTile.jsx
+++ src/components/tiles/trackTile.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import { toString } from "lodash-es";
+import { FaPlay } from "react-icons/fa";
 
 const TrackTile = ({ data }) => {
+  const handlePlayTrack = (data) => {
+    // TODO dominick
+    console.log(data);
+  };
   if (data) {
     return (
       <>
-        <div className="shrink-0 w-48 overflow-hidden bg-transparent">
+        <div className="relative shrink-0 w-48 overflow-hidden bg-transparent hover:">
           {data?.album?.images?.length > 0 &&
           toString(data?.album?.images[0]?.url).trim() !== "" ? (
             <img
@@ -29,6 +34,13 @@ const TrackTile = ({ data }) => {
             <span className="block text-sm text-gray-700 dark:text-gray-200 truncate">
               {`Song - ${data?.artists[0]?.name}`}
             </span>
+          </div>
+          <div className="absolute bottom-0 left-0 right-0 top-0 h-48 w-48 overflow-hidden bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 opacity-0 transition duration-300 ease-in-out hover:opacity-80">
+            <div className="text-3xl text-white text-center flex justify-center align-center items-center w-full h-full">
+              <button onClick={() => handlePlayTrack(data)} type="button">
+                <FaPlay />
+              </button>
+            </div>
           </div>
         </div>
       </>
