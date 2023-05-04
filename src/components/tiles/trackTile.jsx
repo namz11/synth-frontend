332dfaaa98bced4657cd4f95d1801c1f63142bca
Dominick Varano <46766544+PaperPony@users.noreply.github.com>
2023-05-04 15:47:48 -0400

    Added music playing for tracks in search and on album pages

--- src/components/tiles/trackTile.jsx
+++ src/components/tiles/trackTile.jsx
@@ -1,13 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import Link from "next/link";
 import { toString } from "lodash-es";
 import { FaPlay } from "react-icons/fa";
+import { spotifyApi } from "react-spotify-web-playback";
+import { PlayerContext } from "@context/PlayerContext";
+import axios from "axios";
 
-const TrackTile = ({ data }) => {
-  const handlePlayTrack = (data) => {
-    // TODO dominick
-    console.log(data);
-  };
+const TrackTile = ({ data, token }) => {
+  const [deviceId, setDeviceId] = useContext(PlayerContext);
+
+  async function handlePlayerAdd(trackUriList, token) {
+    let spotifyToken;
+    console.log(token);
+    const getSpotifyToken = async () => {
+      spotifyToken = await axios(`/api/token`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    };
+    await getSpotifyToken();
+    spotifyToken = spotifyToken.data.token;
+
+    // Get the device id of the spotify player from context.
+    console.log(trackUriList);
+    await spotifyApi.play(spotifyToken, {
+      uris: trackUriList,
+      deviceId: deviceId,
+    });
+  }
 
   if (data) {
     return (
@@ -38,7 +59,10 @@ const TrackTile = ({ data }) => {
           </div>
           <div className="absolute bottom-0 left-0 right-0 top-0 h-48 w-48 overflow-hidden bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 opacity-0 transition duration-300 ease-in-out hover:opacity-80">
             <div className="text-3xl text-white text-center flex justify-center align-center items-center w-full h-full">
-              <button onClick={() => handlePlayTrack(data)} type="button">
+              <button
+                onClick={() => handlePlayerAdd([data.uri], token)}
+                type="button"
+              >
                 <FaPlay />
               </button>
             </div>
